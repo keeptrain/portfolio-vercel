1a6a01d9f1b1b09a4d306b5fe9b5a1311e4cb72e
keeptrain <19211020@bsi.ac.id>
2025-08-16 20:09:24 +0700

    optimize layout Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,39 +8,27 @@ const Hero = () => {
   const {t} = useLanguage()
 
   return (
-    <div className="container-max section-padding">
-      <div className="z-1 bg-white dark:bg-gray-950 border-l border-r pl-12 pr-12">
-        <section className="min-h-screen flex items-center">
-          <div className="">
-            <ProfileImage/>
-            <div className="animate-fade-in">
-              <h1 className="sm:text-5xl lg:text-6xl font-bold text-gray-900 dark:text-gray-100 mb-6">
-                {t('hero.greeting')}{' '}
-                <span
-                  className="ml-2 text-primary-600 bg-gradient-to-r from-primary-500 to-primary-700 bg-clip-text text-transparent gap-2">
+    <section id="hero" className="container-max section-padding">
+      <div className="min-h-screen flex items-center px-12 bg-white dark:bg-gray-950 border-l border-r">
+        <div className="animate-slide-up">
+          <ProfileImage/>
+          <h1 className="text-xl sm:text-5xl lg:text-6xl font-bold text-gray-900 dark:text-gray-100 mb-6">
+            {t('hero.greeting')}{' '}
+            <span
+              className="ml-2 text-primary-600 bg-gradient-to-r from-primary-500 to-primary-700 bg-clip-text text-transparent gap-2">
                   {t('hero.name')}
                   </span>
-              </h1>
+          </h1>
 
-              <div className="flex">
-                <h2 className="sm:text-5xl lg:text-1xl font-bold text-gray-200 dark:text-gray-100 mb-6">
-                  {t('hero.role.greeting')} <span className='text-gray-900'>{t('hero.role')}___________________.</span>
-                </h2>
-              </div>
-              <OpenToWorkBadge/>
-            </div>
-
-            <div
-              className="pointer-events-note absolute inset-0 -z-10 bg-[radial-gradient(ellipse_at_center,rgba(255,215,0,0.12),transparent_1%)]"/>
-            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-              <div className="w-6 h-10 border-2 border-gray-300 dark:border-gray-600 rounded-full flex justify-center">
-                <div className="w-1 h-3 bg-gray-400 dark:bg-gray-500 rounded-full mt-2 animate-pulse"></div>
-              </div>
-            </div>
+          <div className="flex">
+            <h2 className="sm:text-5xl lg:text-1xl font-bold text-gray-200 dark:text-gray-100 mb-6">
+              {t('hero.role.greeting')} <span className='text-gray-900'>{t('hero.role')}___________________.</span>
+            </h2>
           </div>
-        </section>
+          <OpenToWorkBadge/>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
