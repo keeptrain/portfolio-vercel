63a6991dbbbb48f15e0f4e9e85306ef5de244b40
keeptrain <19211020@bsi.ac.id>
2025-09-15 08:02:28 +0700

    update About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,69 +1,77 @@
 'use client'
 
-import {useState} from "react";
 import {useLanguage} from '@/contexts/LanguageContext'
-import ExperienceTimeline from "@/components/_/ExperienceTimeline";
-import ExperienceDescription from "@/components/_/ExperienceDescription";
-import HowIWork from "@/components/_/HowIWork";
+import SectionContainer from "@/components/_/SectionContainer";
+import HowIWork from "@/components/about/HowIWork";
+import SkillCards from "@/components/ui/SkillCards";
+import {DesktopComputer, DevicePhoneMobile, RectangleGroup} from "@/components/icons/heroicons";
+
+const skillsData = [
+  {
+    icon: <RectangleGroup color={"text-white dark:text-chartreuse"}/>,
+    title: "Responsive Design",
+    description: "Designing web pages that look good on and perform equally well on all devices screen sizes.",
+  },
+  {
+    icon: <DesktopComputer color={"text-white dark:text-chartreuse"}/>,
+    title: "Web",
+    description: "Designing web pages that look good on and perform equally well on all devices screen sizes.",
+  },
+  {
+    icon: <DevicePhoneMobile color={"text-white dark:text-chartreuse"}/>,
+    title: "Mobile apps",
+    description: "Designing web pages that look good on and perform equally well on all devices screen sizes.",
+  },
+  // {
+  //   icon: <MultiStarts color={"text-chartreuse"}/>,
+  //   title: "More skills",
+  //   description: "Hungryyy!",
+  // }
+]
 
 const About = () => {
   const {t} = useLanguage()
-  const [selectedTimeline, setSelectedTimeline] = useState<number | null>(null)
-
-  const handleSelectedTimeline =
-    (index: number) => () => {
-      setSelectedTimeline(index);
-    };
 
   return (
-    <section id="about" className="min-h-screen grid grid-cols-1 lg:grid-rows-[50vh_50vh]">
-      {/*Top grid*/}
-      <div className="bg-white border-b min-h-[50vh] lg:h-[50vh] lg:max-h-[50vh]">
-        {/*My experience overline border*/}
-        <div className="overline-border">
-          <div className="container-max section-padding">
-              <span
-                className="heading-mono text-sky-500 lg:px-12">
-                My Experience
-              </span>
-          </div>
+    <section id="about" className="min-h-screen bg-zinc-50 dark:bg-[#080808]">
+      <div className="grid grid-rows-1 lg:grid-rows-2 md:h-screen gap-4">
+        <div className="flex items-center mt-12">
+          <SectionContainer width="7xl">
+            <div
+              className="flex flex-col items-center justify-center h-50 hover:scale-105 transition-transform duration-300">
+              <h1
+                className="text-xl font-bold font-mono uppercase text-gray-400 dark:text-white ">
+                What can I do?
+              </h1>
+              <h1
+                className="text-xl font-bold font-mono uppercase text-black dark:text-chartreuse ">
+                What can I do?
+              </h1>
+            </div>
+            <div className="grid grid-rows-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {skillsData.map((item, index) => (
+                <div
+                  key={index}
+                  className="col-span-1 backdrop-blur-md bg-black  dark:bg-[#ccf381]/10 border border-[#ccf381]/20 rounded-2xl p-6 shadow-sm
+                  flex flex-col justify-center items-start px-6 pt-10 pb-6 h-50 space-y-2 ">
+                  <SkillCards
+                    icons={item.icon}
+                    description={item.description} title={item.title}/>
+                </div>
+              ))}
+            </div>
+          </SectionContainer>
         </div>
-
-        {/*3 grid at top grid*/}
-        <div className="container-max section-padding grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-0">
-          {/* Left column */}
-          <div className="col-span-1 order-1 lg:order-none">
-            <ExperienceTimeline
-              onItemClick={handleSelectedTimeline}
-            />
-          </div>
-          {/* Right column */}
-          <div className="col-span-1 lg:col-span-2 flex items-center py-4 lg:py-8 order-2 lg:order-none">
-            <ExperienceDescription
-              index={selectedTimeline}
-              onClear={() => setSelectedTimeline(null)}
-            />
-          </div>
-        </div>
-      </div>
-
-      {/*Bottom grid*/}
-      <div className="relative min-h-[50vh] lg:h-[50vh] lg:max-h-[50vh]">
-        {/* Label selalu di atas */}
-        <div className="overline-border lg:top-[67px]">
-          <div className="container-max section-padding">
-            <span className="heading-mono text-fuchsia-500 lg:px-12">
+        <div className="flex flex-col items-center justify-center overflow-x-scroll scrollbar-hide">
+          <SectionContainer>
+            <h1
+              className="flex text-xl font-bold font-mono uppercase text-black dark:text-chartreuse">
               How I Work
-            </span>
-          </div>
-        </div>
-
-        {/* Konten yang selalu center secara vertical */}
-        <div className="container-max section-padding flex items-center min-h-[calc(44vh)] py-4 lg:py-0">
-          <HowIWork/>
+            </h1>
+            <HowIWork/>
+          </SectionContainer>
         </div>
       </div>
-
     </section>
   )
 }
