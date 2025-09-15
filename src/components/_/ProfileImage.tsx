9a2c7e58875b4c6517ff4809eb7d68eaf5045111
keeptrain <19211020@bsi.ac.id>
2025-09-15 08:02:16 +0700

    update Hero section

--- src/components/_/ProfileImage.tsx
+++ src/components/_/ProfileImage.tsx
@@ -1,26 +1,14 @@
 import Image from "next/image";
 
 export default function ProfileImage() {
-    return (
-        <div
-            className="
-        inline-block
-        rounded-[30%]
-        p-1
-        bg-white
-        shadow-md
-        mb-6
-      "
-            style={{
-                boxShadow: "0 2px 10px rgba(0,0,0,0.08)"
-            }}
-        >
-            <Image
-                src="/img.png"
-                alt="Profile"
-                width={100} height={100}
-                className="rounded-[30%] object-cover"
-            />
-        </div>
-    );
+  return (
+    <div className="relative h-full w-full">
+      <Image
+        src="/photos.png"
+        alt="Profile"
+        fill
+        className="object-cover object-top"
+      />
+    </div>
+  );
 }
