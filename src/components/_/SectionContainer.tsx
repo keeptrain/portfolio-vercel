a8e6e589f3b3c4833c72a766dad620bdd6e8bafa
keeptrain <19211020@bsi.ac.id>
2025-09-15 07:56:21 +0700

    add SectionContainer.tsx

---
+++ src/components/_/SectionContainer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface Props {
+  width?: string;
+  children: React.ReactNode;
+}
+
+const SectionContainer = ({width, children}: Props) => {
+  const maxWidthClass = width ? `max-w-${width}` : 'max-w-7xl';
+
+  return (
+    <div className={`${maxWidthClass} mx-auto px-4 sm:px-8 md:px-12`}>
+      {children}
+    </div>
+  )
+}
+
+export default SectionContainer
