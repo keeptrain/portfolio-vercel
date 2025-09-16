b2e928dd1d4b9e0ce4a4507f824d300e018f0a2f
keeptrain <19211020@bsi.ac.id>
2025-09-16 14:37:46 +0700

    rename devicons.jsx and heroicons.jsx to DevIcons.jsx and HeroIcons.jsx; add LaravelWithText and ArrowsUpDown components; adjust SVG sizes

--- src/components/icons/HeroIcons.jsx
+++ src/components/icons/HeroIcons.jsx
@@ -9,10 +9,18 @@ export const ArrowLeft = ({color}) => {
     )
 }
 
+export const ArrowsUpDown = ({color}) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+        </svg>
+    )
+}
+
 export const Bars3BottomRight = ({color}) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-             className="size-8">
+             className="size-6 md:size-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"/>
         </svg>
 
@@ -72,7 +80,7 @@ export const HandThumbUp = ({color}) => {
 export const MultiStarts = ({color}) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-             stroke="currentColor" className={`size-6 ${color}`}>
+             stroke="currentColor" className={`size-6 md:size-8 ${color}`}>
             <path strokeLinecap="round" strokeLinejoin="round"
                   d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"/>
         </svg>
