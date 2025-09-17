8a1d0bde39da3952eaaaf0f23dd2f95ba6d1b4d5
keeptrain <19211020@bsi.ac.id>
2025-09-17 19:18:56 +0700

    refactor: HeroIcons.jsx

--- src/components/icons/HeroIcons.jsx
+++ src/components/icons/HeroIcons.jsx
@@ -11,7 +11,7 @@ export const ArrowLeft = ({color}) => {
 
 export const ArrowUp = ({color}) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 md:size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
 
