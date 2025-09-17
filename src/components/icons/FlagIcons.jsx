aeef0bc937bfe7e1703beee99ea151c44ec81785
keeptrain <19211020@bsi.ac.id>
2025-09-17 17:17:46 +0700

    add ArrowUp icon and update IndonesiaFlag component for consistent sizing

--- src/components/icons/FlagIcons.jsx
+++ src/components/icons/FlagIcons.jsx
@@ -1,7 +1,7 @@
 
 export const IndonesiaFlag = ({color}) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="size-6">
             <path d="M31,8c0-2.209-1.791-4-4-4H5c-2.209,0-4,1.791-4,4v9H31V8Z" fill="#ea3323"></path>
             <path d="M5,28H27c2.209,0,4-1.791,4-4v-8H1v8c0,2.209,1.791,4,4,4Z" fill="#fff"></path>
             <path d="M5,28H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4ZM2,8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8Z" opacity=".15"></path>
