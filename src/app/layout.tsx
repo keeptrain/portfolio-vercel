a4b0c5a70cdf53978f18dabf5ca1398a86934db1
keeptrain <chefgilang2@gmail.com>
2026-08-13 13:48:21 +0700

    fix: warning metadataBase property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 
+const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL
+  ? process.env.NEXT_PUBLIC_SITE_URL
+  : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "KeepTrain",
   description: "A modern, minimalist portfolio showcasing my work and skills.",
   robots: {
