309001fa9a5024be5365685c4a96e5534955d83b
keeptrain <chefgilang2@gmail.com>
2026-08-20 16:58:39 +0700

    refactor: simplify ProjectList to static list

--- src/features/projects/components/ProjectList.tsx
+++ src/features/projects/components/ProjectList.tsx
@@ -1,41 +1,20 @@
 "use client";
 
-import { useState } from "react";
 import { Project } from "../data/projects";
 import ProjectAdapter from "./ProjectAdapter";
-import ProjectDetailDrawer from "./ProjectDetailDrawer";
 
 interface ProjectListProps {
   projects: Project[];
 }
 
 export default function ProjectList({ projects }: ProjectListProps) {
-  const [selectedProject, setSelectedProject] = useState<Project>(projects[0]);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleSelectProject = (project: Project) => {
-    setSelectedProject(project);
-    setIsOpen(true);
-  };
-
   return (
     <>
-      <div className="flex flex-col gap-4 divide-y divide-gray-300 dark:divide-zinc-700 pb-6">
+      <div className="flex flex-col gap-4 divide-y divide-gray-300 pb-6 dark:divide-zinc-700">
         {projects.map((project, index) => (
-          <ProjectAdapter
-            key={project.slug || index}
-            project={project}
-            onClick={() => handleSelectProject(project)}
-          />
+          <ProjectAdapter key={project.slug || index} project={project} />
         ))}
       </div>
-
-      {/* Single Controlled Drawer Instance */}
-      <ProjectDetailDrawer
-        project={selectedProject}
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-      />
     </>
   );
 }
