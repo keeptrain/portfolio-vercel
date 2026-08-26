579315ccf39b1dc90193c8a16820c89b64043792
keeptrain <chefgilang2@gmail.com>
2026-08-26 15:07:06 +0700

    style: refactor section two layout to be responsive using flex-col and media queries

--- src/features/landing/components/SectionTwo.tsx
+++ src/features/landing/components/SectionTwo.tsx
@@ -6,7 +6,7 @@ export default function SectionTwo() {
   const t = getT();
   return (
     <section id="section-2" className="my-8 sm:my-20 md:py-6">
-      <div className="mx-4 mb-15 flex items-baseline gap-3">
+      <div className="mx-4 mb-15 flex flex-col gap-2 sm:items-baseline sm:gap-3 md:flex-row">
         <h2 className="text-4xl font-semibold">
           {t("sectionTwo.howIWork.title")}
         </h2>
